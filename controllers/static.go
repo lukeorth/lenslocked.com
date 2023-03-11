aad5c1320e574104179104a09a673bd0a8ab50ab
Luke Orth <lorth001@gmail.com>
2023-03-10 19:53:04 -0600

    Created a controller for our static pages, and we also updated the View type to implement the http.Handler interface

---
+++ controllers/static.go
@@ -0,0 +1,15 @@
+package controllers
+
+import "github.com/lukeorth/lenslocked.com/views"
+
+func NewStatic() *Static {
+    return &Static{
+        Home: views.NewView("bootstrap", "views/static/home.html"),
+        Contact: views.NewView("bootstrap", "views/static/contact.html"),
+    }
+}
+
+type Static struct {
+    Home *views.View
+    Contact *views.View
+}
--- main.go
+++ main.go
@@ -5,32 +5,15 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/lukeorth/lenslocked.com/controllers"
-	"github.com/lukeorth/lenslocked.com/views"
 )
-
-var (
-    homeView *views.View
-    contactView *views.View
-)
-
-func home(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html")
-    must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html")
-    must(contactView.Render(w, nil))
-}
 
 func main() {
-    homeView = views.NewView("bootstrap", "views/home.html")
-    contactView = views.NewView("bootstrap", "views/contact.html")
+    staticC := controllers.NewStatic()
     usersC := controllers.NewUsers()
 
     r := mux.NewRouter()
-    r.HandleFunc("/", home).Methods("GET")
-    r.HandleFunc("/contact", contact).Methods("GET")
+    r.Handle("/", staticC.Home).Methods("GET")
+    r.Handle("/contact", staticC.Contact).Methods("GET")
     r.HandleFunc("/signup", usersC.New).Methods("GET")
     r.HandleFunc("/signup", usersC.Create).Methods("POST")
     must(http.ListenAndServe(":3000", r))
