395ba5f87350f52d63a7fb1f16bdfa1e3cd8de45
Luke Orth <lorth001@gmail.com>
2023-03-21 14:59:41 -0500

    Updated the create action on the users controller to sign in the user after an account is created

--- controllers/users.go
+++ controllers/users.go
@@ -60,7 +60,8 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
-    fmt.Fprintln(w, user)
+    signIn(w, &user)
+    http.Redirect(w, r, "/cookietest", http.StatusFound)
 }
 
 type LoginForm struct {
@@ -91,13 +92,17 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+    signIn(w, user)
+    http.Redirect(w, r, "/cookietest", http.StatusFound)
+}
+
+// signIn is used to sign the given user in via cookies
+func signIn(w http.ResponseWriter, user *models.User) {
     cookie := http.Cookie{
         Name: "email",
         Value: user.Email,
     }
     http.SetCookie(w, &cookie)
-    fmt.Println(w, user)
-
 }
 
 // CookieTest is used to display cookies on the current user
