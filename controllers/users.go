67274a711c9034d677f730e0f3b311b7eda94550
Luke Orth <lorth001@gmail.com>
2023-03-24 20:38:16 -0500

    Rendered a dynamic alert

--- controllers/users.go
+++ controllers/users.go
@@ -32,7 +32,15 @@ type Users struct {
 //
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-    if err := u.NewView.Render(w, nil); err != nil {
+    type Alert struct {
+        Level string
+        Message string
+    }
+    a := Alert{
+        Level: "warning",
+        Message: "successfully rendered a dynamic alert!",
+    }
+    if err := u.NewView.Render(w, a); err != nil {
         panic(err)
     }
 }
