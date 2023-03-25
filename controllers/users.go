af6cfda6ef3535bdc9527150698bc5f979f08b35
Luke Orth <lorth001@gmail.com>
2023-03-24 21:16:00 -0500

    Added a views.Data type to define top level structure for view data

--- controllers/users.go
+++ controllers/users.go
@@ -32,21 +32,11 @@ type Users struct {
 //
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-    type Alert struct {
-        Level string
-        Message string
-    }
-    type Data struct {
-        Alert Alert
-        Yield interface{}
-    }
-    a := Alert{
-        Level: "warning",
-        Message: "successfully rendered a dynamic alert!",
-    }
-    d := Data{
-        Alert: a,
-        Yield: "hello!",
+    d := views.Data{
+        Alert: &views.Alert{
+            Level: views.AlertLvlError,
+            Message: "something went wrong",
+        },
     }
     if err := u.NewView.Render(w, d); err != nil {
         panic(err)
