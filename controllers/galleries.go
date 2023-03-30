3df193b405ca5d7420078b6c2f9d62f8db68a043
Luke Orth <lorth001@gmail.com>
2023-03-30 15:00:47 -0500

    Added the Create action to the Galleries controller

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
-    "github.com/lukeorth/lenslocked.com/models"
-    "github.com/lukeorth/lenslocked.com/views"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/lukeorth/lenslocked.com/models"
+	"github.com/lukeorth/lenslocked.com/views"
 )
 
 func NewGalleries(gs models.GalleryService) *Galleries {
@@ -15,5 +19,30 @@ func NewGalleries(gs models.GalleryService) *Galleries {
 type Galleries struct {
     New *views.View
     gs models.GalleryService
+}
+
+type GalleryForm struct {
+    Title string `schema:"title"`
+}
+
+// POST /galleries
+func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
+    var vd views.Data
+    var form GalleryForm
+    if err := parseForm(r, &form); err != nil {
+        log.Println(err)
+        vd.SetAlert(err)
+        g.New.Render(w, vd)
+        return
+    }
+    gallery := models.Gallery{
+        Title: form.Title,
+    }
+    if err := g.gs.Create(&gallery); err != nil {
+        vd.SetAlert(err)
+        g.New.Render(w, vd)
+        return
+    }
+    fmt.Fprintln(w, gallery)
 }
 
