f471762659622e4f61747cdc419d02dc15c6631e
Luke Orth <lorth001@gmail.com>
2023-04-05 05:34:01 -0500

    Updated our code to always query for the current user and to set the user on the view data if the user exists

--- controllers/users.go
+++ controllers/users.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -33,7 +32,7 @@ type Users struct {
 //
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-    u.NewView.Render(w, nil)
+    u.NewView.Render(w, r, nil)
 }
 
 type SignupForm struct {
@@ -52,7 +51,7 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
     if err := parseForm(r, &form); err != nil {
         log.Println(err)
         vd.SetAlert(err)
-        u.NewView.Render(w, vd)
+        u.NewView.Render(w, r, vd)
         return
     }
     user := models.User{
@@ -62,7 +61,7 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
     }
     if err := u.us.Create(&user); err != nil {
         vd.SetAlert(err)
-        u.NewView.Render(w, vd)
+        u.NewView.Render(w, r, vd)
         return
     }
     err := u.signIn(w, &user)
@@ -70,7 +69,7 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
         http.Redirect(w, r, "/login", http.StatusFound)
         return
     }
-    http.Redirect(w, r, "/cookietest", http.StatusFound)
+    http.Redirect(w, r, "/galleries", http.StatusFound)
 }
 
 type LoginForm struct {
@@ -88,7 +87,7 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
     if err := parseForm(r, &form); err != nil {
         log.Println(err)
         vd.SetAlert(err)
-        u.LoginView.Render(w, vd)
+        u.LoginView.Render(w, r, vd)
         return
     }
 
@@ -100,17 +99,17 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
         default:
             vd.SetAlert(err)
         }
-        u.LoginView.Render(w, vd)
+        u.LoginView.Render(w, r, vd)
         return
     }
 
     err = u.signIn(w, user)
     if err != nil {
         vd.SetAlert(err)
-        u.LoginView.Render(w, vd)
+        u.LoginView.Render(w, r, vd)
         return
     }
-    http.Redirect(w, r, "/cookietest", http.StatusFound)
+    http.Redirect(w, r, "/galleries", http.StatusFound)
 }
 
 // signIn is used to sign the given user in via cookies
@@ -134,19 +133,4 @@ func (u *Users) signIn(w http.ResponseWriter, user *models.User) error {
     }
     http.SetCookie(w, &cookie)
     return nil
-}
-
-// CookieTest is used to display cookies on the current user
-func (u *Users) CookieTest(w http.ResponseWriter, r *http.Request) {
-    cookie, err := r.Cookie("remember_token")
-    if err != nil {
-        http.Redirect(w, r, "/login", http.StatusFound)
-        return
-    }
-    user, err := u.us.ByRemember(cookie.Value)
-    if err != nil {
-        http.Redirect(w, r, "/login", http.StatusFound)
-        return
-    }
-    fmt.Fprintf(w, "%+v", user)
 }
--- middleware/require_user.go
+++ middleware/require_user.go
@@ -7,24 +7,24 @@ import (
 	"github.com/lukeorth/lenslocked.com/models"
 )
 
-type RequireUser struct {
+type User struct {
     models.UserService
 }
 
-func (mw *RequireUser) Apply(next http.Handler) http.HandlerFunc {
+func (mw *User) Apply(next http.Handler) http.HandlerFunc {
     return mw.ApplyFn(next.ServeHTTP)
 }
 
-func (mw *RequireUser) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
+func (mw *User) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
         cookie, err := r.Cookie("remember_token")
         if err != nil {
-            http.Redirect(w, r, "/login", http.StatusFound)
+            next(w, r)
             return
         }
         user, err := mw.UserService.ByRemember(cookie.Value)
         if err != nil {
-            http.Redirect(w, r, "/login", http.StatusFound)
+            next(w, r)
             return
         }
         ctx := r.Context()
@@ -32,4 +32,29 @@ func (mw *RequireUser) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
         r = r.WithContext(ctx)
         next(w, r)
     })
+}
+
+// RequireUser assumes that User middleware has already been run
+// otherwise it will not work correctly.
+type RequireUser struct {
+    User
+}
+
+// Apply assumes that User middleware has already been run
+// otherwise it will not work correctly.
+func (mw *RequireUser) Apply(next http.Handler) http.HandlerFunc {
+    return mw.ApplyFn(next.ServeHTTP)
+}
+
+// RequireUser assumes that User middleware has already been run
+// otherwise it will not work correctly.
+func (mw *RequireUser) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
+    return http.HandlerFunc((func(w http.ResponseWriter, r *http.Request){
+        user := context.User(r.Context())
+        if user == nil {
+            http.Redirect(w, r, "/login", http.StatusFound)
+            return
+        }
+        next(w, r)
+    }))
 }
